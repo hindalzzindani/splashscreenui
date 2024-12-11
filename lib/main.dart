90248e3d79a03f9754b8b64222ad8b97b514ee0f
hindalzzindani <hindalzzindani@gmail.com>
2024-12-11 10:39:04 +0300

    Best Practices for Creating an Effective Splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:splash_screen/splash_screen2.dart';
-import 'package:splash_screen/splash_screen3.dart';
-import 'package:splash_screen/splash_screen_moving.dart';
-import 'package:splash_screen/splash_screenwav.dart';
-import 'splash_screen.dart';
+import 'package:splash_screen/splash_screens/splash_screen2.dart';
+import 'package:splash_screen/splash_screens/splash_screen3.dart';
+import 'package:splash_screen/splash_screens/splash_screen_moving.dart';
+import 'package:splash_screen/splash_screens/splash_screen_moving.dart';
+import 'package:splash_screen/splash_screens/splash_screenwav.dart';
+import 'splash_screens/splash_screen.dart';
 
 void main() {
   runApp(MyApp());
