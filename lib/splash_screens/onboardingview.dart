59a0a917abac49180b78594dcb4029b58da53233
hindalzzindani <hindalzzindani@gmail.com>
2024-12-12 17:03:16 +0300

    Splash new onboaradd

--- lib/splash_screens/onboardingview.dart
+++ lib/splash_screens/onboardingview.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
-
 class OnboardingView extends StatefulWidget {
   const OnboardingView({Key? key}) : super(key: key);
 
